65c7be773053341218fad05179c79dd947088983
Seyi Oyemade <seyioyemade918@gmail.com>
2023-05-05 09:36:08 +0100

    Fix linters error

--- src/tests/Home.test.js
+++ src/tests/Home.test.js
@@ -1,6 +1,6 @@
 import renderer from 'react-test-renderer';
-import Home from '../components/Home';
 import { BrowserRouter } from 'react-router-dom';
+import Home from '../components/Home';
 
 it('renders correctly', () => {
   const tree = renderer.create(<BrowserRouter><Home /></BrowserRouter>).toJSON();
